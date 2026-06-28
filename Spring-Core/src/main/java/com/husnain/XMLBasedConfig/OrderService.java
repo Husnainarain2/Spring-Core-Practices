6bbf5d4fe4a6fdd264d874d90e073cf0d3ddbd81
Husnainarain2 <husnainabid9494@gmail.com>
2026-06-28 09:27:21 +0100

    Create a bean of OrderService object with XML based congifuartion

---
+++ Spring-Core/src/main/java/com/husnain/XMLBasedConfig/OrderService.java
@@ -0,0 +1,10 @@
+package com.husnain.XMLBasedConfig;
+
+public class OrderService {
+    public OrderService() {
+        System.out.println("OrderService instance created.");
+    }
+    public void placeOrder() {
+        System.out.println("Order placed successfully!");
+    }
+}
--- Spring-Core/src/main/java/com/husnain/XMLBasedConfig/Run.java
+++ Spring-Core/src/main/java/com/husnain/XMLBasedConfig/Run.java
@@ -6,6 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Run {
     public static void main(String[] args) {
         ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+//        OrderService orderService = (OrderService) context.getBean("orderService");
+//        OrderService orderService2= context.getBean(OrderService.class);
+        OrderService orderService3=context.getBean("orderService", OrderService.class);
+        orderService3.placeOrder();
 
     }
 }
