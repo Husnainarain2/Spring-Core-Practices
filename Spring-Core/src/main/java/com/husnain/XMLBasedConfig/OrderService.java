ece579de35806c8ad7f9dd5d4c97aa245cd7afc6
Husnainarain2 <husnainabid9494@gmail.com>
2026-06-28 09:54:28 +0100

    Create a file of payment service and inject dependency of order service

--- Spring-Core/src/main/java/com/husnain/XMLBasedConfig/OrderService.java
+++ Spring-Core/src/main/java/com/husnain/XMLBasedConfig/OrderService.java
@@ -1,10 +1,14 @@
 package com.husnain.XMLBasedConfig;
 
 public class OrderService {
-    public OrderService() {
+    private PaymentService paymentService;
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
         System.out.println("OrderService instance created.");
     }
+
     public void placeOrder() {
         System.out.println("Order placed successfully!");
+        paymentService.processPayment();
     }
 }
---
+++ Spring-Core/src/main/java/com/husnain/XMLBasedConfig/PaymentService.java
@@ -0,0 +1,11 @@
+package com.husnain.XMLBasedConfig;
+
+public class PaymentService {
+
+    private PaymentService() {
+        System.out.println("PaymentService instance created.");
+    }
+    public void processPayment() {
+        System.out.println("Payment processed successfully!");
+    }
+}
