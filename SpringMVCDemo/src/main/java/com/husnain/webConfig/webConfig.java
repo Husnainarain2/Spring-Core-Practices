b339cfb396d13c703e505df754eae3006e62b14c
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-10 11:45:38 +0100

    Add congif file

---
+++ SpringMVCDemo/src/main/java/com/husnain/webConfig/webConfig.java
@@ -0,0 +1,11 @@
+package com.husnain.webConfig;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configurable
+@ComponentScan
+@EnableWebMvc
+public class webConfig {
+}
