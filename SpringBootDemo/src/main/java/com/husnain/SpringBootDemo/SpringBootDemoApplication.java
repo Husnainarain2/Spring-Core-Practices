8544fc06a02b0350d2d317dcb21d5b0c9dfb0740
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-10 10:25:09 +0100

    Create new Demo project for core spring mvc practices without springboot

--- SpringBootDemo/src/main/java/com/husnain/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/husnain/SpringBootDemo/SpringBootDemoApplication.java
@@ -2,14 +2,13 @@ package com.husnain.SpringBootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 /*@ConfigurationPropertiesScan()*/
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+        SpringApplication.run(SpringBootDemoApplication.class, args);
 
 
 //        paymentGateway Gateway = context.getBean(paymentGateway.class);
