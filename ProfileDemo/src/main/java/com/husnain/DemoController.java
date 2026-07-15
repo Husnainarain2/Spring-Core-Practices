330f72fc74ef977761014a7b75264025ed408e28
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-15 16:44:22 +0100

    feat(ProfileDemo): add Spring Boot application properties for configuration and internationalization fix(ProfileDemo): correct typo in Main.java static void main() method refactor(ProfileDemo): refactor DemoController to use @Value annotations for configuration values

--- ProfileDemo/src/main/java/com/husnain/DemoController.java
+++ ProfileDemo/src/main/java/com/husnain/DemoController.java
@@ -1,16 +1,20 @@
 package com.husnain;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/Demo")
 public class DemoController {
-
-    private String name;
+    @Value("${app.welcome.users}")
+    private List<String> name;
+    @Value("${app.welcome.message}")
     private String message;
 
     @GetMapping
