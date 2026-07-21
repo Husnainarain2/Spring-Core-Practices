ef8dba0d0a55b459a8bd3758293a58254c219dd7
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-21 20:10:45 +0100

    feat(filterDemo/src/main/java/com/example/filterDemo/handlerInterceptor/AuthenticationInterceptor.java): add authentication interceptor to check API key in request headers

--- filterDemo/src/main/java/com/example/filterDemo/handlerInterceptor/AuthenticationInterceptor.java
+++ filterDemo/src/main/java/com/example/filterDemo/handlerInterceptor/AuthenticationInterceptor.java
@@ -1,4 +1,20 @@
 package com.example.filterDemo.handlerInterceptor;
 
-public class AuthenticationInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            String apiKey =request.getHeader("x-api" +
+                    "-key");
+            if(apiKey != null && !apiKey.equals("secret123")) {
+                return false;
+            }
+            return true;
+    }
 }
