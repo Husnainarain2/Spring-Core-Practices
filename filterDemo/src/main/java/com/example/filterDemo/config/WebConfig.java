208d081f9f657c5742ff75cd0eeaad02840fc4d1
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-21 20:11:19 +0100

    fix(WebConfig.java): fix constructor parameter order to match field declaration feat(WebConfig.java): add AuthenticationInterceptor as a new interceptor and set its order

--- filterDemo/src/main/java/com/example/filterDemo/config/WebConfig.java
+++ filterDemo/src/main/java/com/example/filterDemo/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.filterDemo.config;
 
+import com.example.filterDemo.handlerInterceptor.AuthenticationInterceptor;
 import com.example.filterDemo.handlerInterceptor.LoggingInterceptor;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,11 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoggingInterceptor loggingInterceptor;
-    public WebConfig(LoggingInterceptor loggingInterceptor) {
+    private final AuthenticationInterceptor  authenticationInterceptor;
+    public WebConfig(LoggingInterceptor loggingInterceptor,
+                     AuthenticationInterceptor authenticationInterceptor) {
         this.loggingInterceptor = loggingInterceptor;
+        this.authenticationInterceptor= authenticationInterceptor;
     }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loggingInterceptor);
+        registry.addInterceptor(authenticationInterceptor).order(1);
     }
 }
