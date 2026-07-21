79fc511d8e909660af46d103e529d5ab9dc66c5b
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-21 20:08:17 +0100

    feat(filterDemo): add LoggingInterceptor and AuthenticationInterceptor classes to handle logging and authentication respectively refactor(filterDemo): refactor WebConfig class to use constructor injection for LoggingInterceptor

--- filterDemo/src/main/java/com/example/filterDemo/config/WebConfig.java
+++ filterDemo/src/main/java/com/example/filterDemo/config/WebConfig.java
@@ -1,4 +1,19 @@
 package com.example.filterDemo.config;
 
-public class WebConfig {
+import com.example.filterDemo.handlerInterceptor.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configurable
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+    public WebConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
 }
--- filterDemo/src/main/java/com/example/filterDemo/handlerInterceptor/LoggingInterceptor.java
+++ filterDemo/src/main/java/com/example/filterDemo/handlerInterceptor/LoggingInterceptor.java
@@ -1,4 +1,46 @@
 package com.example.filterDemo.handlerInterceptor;
 
-public class LoggingInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        System.out.println("Incoming request..." +
+                "....");
+        System.out.println("Http Method "+ request.getMethod());
+        System.out.println("Http URI "+ request.getRequestURI());
+        System.out.println("Http Host "+ request.getRemoteHost());
+        System.out.println("Client Ip "+ request.getRemoteAddr());
+        System.out.println("Token Header"+ request.getHeader("token"));
+        
+        if(handler instanceof HandlerMethod handlerMethod){
+            System.out.println("Method Name "+ handlerMethod.getMethod().getName());
+            System.out.println("Controller"+ handlerMethod.getBean().getClass().getName());
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("Logging Interceptor" +
+                " postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        System.out.println("Logging Interceptor" +
+                " afterCompletion");
+        System.out.println("Status Code "+ response.getStatus());
+    }
 }
