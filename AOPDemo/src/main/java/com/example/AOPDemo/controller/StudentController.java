514be0e5f0904ffa0b10fd41ca112a77b9b8808c
Husnainarain2 <husnainabid9494@gmail.com>
2026-07-23 19:43:05 +0100

    feat(AOPDemo): add new StudentController and StudentService classes to implement student management functionality

---
+++ AOPDemo/src/main/java/com/example/AOPDemo/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.AOPDemo.controller;
+
+import com.example.AOPDemo.series.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Apo/students")
+public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> create(){
+        String reply=
+                studentService.createStudent();
+        return ResponseEntity.ok(reply);
+    }
+}
