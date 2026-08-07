41e0a84dcaf627667521ec326dd4671be306583e
Husnainarain2 <husnainabid9494@gmail.com>
2026-08-07 12:27:18 +0100

    Create a new demo project where we parctices the JPARelation one to one ,many to one ,many to many ,one to many .....and create a packages and classes of all the layers

---
+++ JPARelationShipDemo/src/main/java/com/example/JpaRelationShipDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelationShipDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRelationShipDemoApplication.class, args);
+	}
+
+}
